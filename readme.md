284eccb8e72c7df58c559af353567f08ca3cb813
RicSti <ricsti77@gmail.com>
2022-06-20 21:38:15 +0200

    initial commit after forking

--- readme.md
+++ readme.md
@@ -1,22 +1,93 @@
+## Description
+
+This Google Apps Script recursively returns all locs of url elements starting from a sitemap file or from a sitemap index file and writes the results to a pre-formatted Google Sheet that can be used as page feed in Google Ads.
+
+This is my very first fork. Feel free to correct or add the information in this README and in the inline comments.
+
+Also I'd be happy for any improvement of my very basic functions. Especially if you find a way to save more execution time. Just as reference: With this script I extracted 620K+ urls from 72 sitemap files in about 23 minutes. 30 minutes is the execution time limit in Google Apps. So this script will probably run into a timeout for bigger website projects with 800K+ indexed urls.
+
 ## Functions
 
 <dl>
-<dt><a href="#sitemap">EXTRACT_SITEMAP(sitemapUrl)</a> ⇒</dt>
-<dd><p>Returns URLs in sitemap.xml index or xml file</p>
+<dt><a href="#main">main()</a> ⇒</dt>
+<dd><p>Sets start and export file urls, prepares cache arrays and calls sub-functions.</p>
+</dd>
+</dl>
+<dl>
+<dt><a href="#fetchSitemaps">fetchSitemaps(url)</a> ⇒</dt>
+<dd><p>Returns sitemap URLs from sitemap index files. Processes sitemap elements only.</p>
+</dd>
+</dl>
+<dl>
+<dt><a href="#fetchXml">fetchXml(url)</a> ⇒</dt>
+<dd><p>Pre-fetches the XML data from all cached sitemap urls to speed up the following extracting process.</p>
+</dd>
+</dl>
+<dl>
+<dt><a href="#extractLocsFromXml">extractLocsFromXml(xml)</a> ⇒</dt>
+<dd><p>Returns url locs from cached sitemap XML contents. Processes url elements only.
+</p>
 </dd>
 </dl>
 
-<a name="sitemap"></a>
+<a name="main"></a>
 
-## EXTRACT_SITEMAP(sitemapUrl) ⇒
-Returns URLs in sitemap.xml file
+## main() ⇒
+Sets start and export file urls, prepares cache arrays and calls sub-functions.
+
+### Process steps:
+
+* Clear existing content in exportSheet
+* Write header to exportSheet
+* Start with url of sitemap or sitemap index file
+* Recursively return all sitemap urls and write them to temp sitemaps array
+* Retrieve the XML content of all sitemaps previously saved in temp sitemaps array
+* Extract all urls (locs) from all cached sitemap contents
+* Finally write all extracted urls (locs) to exportSheet
 
 **Kind**: global function
-**Returns**: Returns urls <loc> from an xml sitemap
 **Customfunction**:
 
 | Param | Type | Description |
 | --- | --- | --- |
-| sitemapUrl | <code>&quot;https://www.google.com/gmail/sitemap.xml&quot;</code> | REQUIRED The url of the sitemap |
+| exportSheetUrl | <code>"https://docs.google.com/spreadsheets/d/..."</code> | REQUIRED  The URL of the Google Sheet to export to
+| startUrl | <code>"https://www.yourdomain.com/sitemap.xml" OR "https://www.yourdomain.com/sitemap-index.xml"</code> |  REQUIRED The url of the sitemap or the sitemap index file
 
-<a name="sitemapIndex"></a>
+<a name="fetchSitemaps"></a>
+
+## fetchSitemaps(url) ⇒
+Returns sitemap URLs from sitemap index files. Processes sitemap elements only.
+
+**Kind**: local variable
+**Customfunction**:
+
+| Param | Type | Description |
+| --- | --- | --- |
+| url | <code>"../sitemap.xml" OR "../sitemap-index.xml </code> | Actually processed sitemap URL provided by main()
+| return | <code>Array</code> |  Extracted sitemap URLs
+
+<a name="fetchXml"></a>
+
+## fetchXml(url) ⇒
+Pre-fetches the XML data from all cached sitemap urls to speed up the following extracting process.
+
+**Kind**: local variable
+**Customfunction**:
+
+| Param | Type | Description |
+| --- | --- | --- |
+| url | <code>"../sitemap.xml" OR "../sitemap-index.xml </code> | Actually processed sitemap URL provided by main()
+| return | <code>Array</code> |  Fetched XML
+
+<a name="extractLocsFromXml"></a>
+
+## extractLocsFromXml(xml) ⇒
+Returns url locs from cached sitemap XML contents. Processes url elements only.
+
+**Kind**: local variable
+**Customfunction**:
+
+| Param | Type | Description |
+| --- | --- | --- |
+| xml | <code>"<?xml version...>"</code> | Actually processed sitemap XML content provided by main()
+| return | <code>Array</code> |  Extracted urls (locs)
--- xmlscript.js
+++ xmlscript.js
@@ -1,5 +1,5 @@
 /* ---------------------------------------------------------------------------
-* Sitemap extractor (Google sheets)
+* Base: Sitemap extractor (Google sheets)
 *
 * @desc    Extracts urls from XML sitemap index or sitemap files
 * @author  Dave Sottimano @dsottimano Twitter
@@ -7,30 +7,150 @@
 * @version 1.0
 * -------------------------------------------------------------------------*/
 
-/**
-* Returns URLs in sitemap.xml file or sitemap index file
+/* ---------------------------------------------------------------------------
+* Fork: Sitemap extractor (Google Apps Script)
 *
-* @param {"https://www.google.com/gmail/sitemap.xml"} sitemapUrl REQUIRED The url of the sitemap
-* @return Returns urls <loc> from an xml sitemap
-* @customfunction
-*/
+* @desc    Recursively returns all locs of url elements starting from sitemap
+* file or from sitemap index file and writes the results to a Google Sheet.
+* @author  RicSti @ric_sti Twitter
+* @version 1.0
+* -------------------------------------------------------------------------*/
 
-function EXTRACT_SITEMAP(sitemapUrl) {
+function main(){
+  
+  /*
+  * Sets start and export file urls, prepares cache arrays and calls sub-functions.
+  *
+  * @param {"https://docs.google.com/spreadsheets/d/..."} exportSheetUrl REQUIRED  The URL of the Google Sheet to export to
+  * @param {"https://www.yourdomain.com/sitemap.xml" | "https://www.yourdomain.com/sitemap-index.xml"} startUrl REQUIRED The url of the sitemap or the sitemap index file
+  */
+ 
+  let exportSheetUrl = ''; // https://docs.google.com/spreadsheets/d/...'
+  let startUrl = ''; // 'https://www.yourwebsite.com/sitemap.xml'  | "https://www.yourdomain.com/sitemap-index.xml"
+
+  var exportSheet = SpreadsheetApp.openByUrl(exportSheetUrl);
+  var sitemapsTemp = [];
+  var sitemapsBatch = [];
+  var contentsBatch = [];
+  var urlsBatch = [];
+
+  // Clear existing content in exportSheet
+  if ( exportSheet.getLastRow() ) { exportSheet.getRange("A1:A" + exportSheet.getLastRow()).clear(); }
+
+  // Write header to exportSheet
+  exportSheet.getRange('A1:B1').setValues([["Page URL","Custom Label"]]);
+
+  // Start with url of sitemap or sitemap index file
+  sitemapsTemp.push(startUrl);
+  sitemapsBatch.push(startUrl);
+
+  // Recursively return all sitemap urls and write them to temp sitemaps array
+  Logger.log("adding sitemap urls to batch...");
+  while (sitemapsTemp.length > 0){
+    var results = fetchSitemaps(sitemapsTemp[0]);
+    for (i=0;i<results.length;i++){ sitemapsTemp.push(results[i]); sitemapsBatch.push(results[i]); }
+    sitemapsTemp.shift();
+  }
+  Logger.log(sitemapsBatch.length + " sitemaps added to sitemaps batch.");
+
+  // Retrieve the XML content of all sitemaps previously saved in temp sitemaps array
+  Logger.log("reading/saving sitemaps as XML to contents batch...");
+  for (i=0;i<sitemapsBatch.length;i++) {
+    contentsBatch.push([fetchXml(sitemapsBatch[i])]);    
+  }
+  Logger.log(contentsBatch.length + " XML contents added to content batch.");
+
+  // Extract all urls (locs) from all cached sitemap contents
+  Logger.log("extracting urls (locs) from sitemap contents...");
+  for (i=0;i<contentsBatch.length;i++) {
+    var results = extractLocsFromXml(contentsBatch[i]);
+    for (j=0;j<results.length;j++){ urlsBatch.push(results[j]); }   
+  }
+  Logger.log(urlsBatch.length + " urls (locs) extracted from sitemap contents.");
+
+  // Finally write all extracted urls (locs) to exportSheet
+  Logger.log("writing urls to sheet...");
+  var range = exportSheet.getRange('A2:A'+(urlsBatch.length+1));
+  range.setValues(urlsBatch);
+
+  Logger.log("Extraction complete. Go to file: " + exportSheetUrl);
+}
+
+function fetchSitemaps(url) {
+
+  /*
+  * Processes sitemap elements only
+  * @return Returns sitemap URLs from sitemap index files
+  */
+
+  var urlFetchOptions = {
+      "muteHttpExceptions": true,      
+      "followRedirects": true
+  };
   try {
-
-    var xml = UrlFetchApp.fetch(sitemapUrl,{muteHttpExceptions:true});
+    var xml = UrlFetchApp.fetch(url,urlFetchOptions);
     var document = XmlService.parse(xml.getContentText());
     var root = document.getRootElement()
     var namespace = root.getNamespace().getURI()
     var sitemapNameSpace = XmlService.getNamespace(namespace);
-    let urls = root.getChildren('url',sitemapNameSpace)[0] ? root.getChildren('url', sitemapNameSpace) : root.getChildren('sitemap', sitemapNameSpace);
-    var locs = []
-    
-    for (var i=0;i <urls.length;i++) {
-      locs.push(urls[i].getChild('loc', sitemapNameSpace).getText()) 
+    var items = [];
+    var sitemaps = [];
+    if ( !root.getChildren('url',sitemapNameSpace)[0] ) {
+      // process only sitemaps
+      items = root.getChildren('sitemap', sitemapNameSpace)
+      for (var i = 0; i < items.length; i++) {
+        sitemaps.push([items[i].getChild('loc', sitemapNameSpace).getText()]) 
+      }
     }
-    
-    return locs  
+    return(sitemaps);
+  } catch (e) {
+    console.log(e)
+    if (e.toString().includes("The markup in the document preceding the root element must be well-formed")) return "Parsing error: is this a valid XML sitemap?";
+    return e.toString() 
+  }
+}
+
+function fetchXml(url) {
+
+  /*
+  * Pre-fetches the XML data from all cached sitemap urls to speed up the following extracting process
+  */
+
+  var urlFetchOptions = {
+      "muteHttpExceptions": true,      
+      "followRedirects": true
+  };
+  try {
+    var xml = UrlFetchApp.fetch(url,urlFetchOptions);
+    return xml;
+  } catch (e) {
+    console.log(e)
+    return e.toString() 
+  }
+}
+
+function extractLocsFromXml(xml){
+
+  /*
+  * Processes url elements only
+  * @return Returns url locs from cached sitemap XML contents
+  */
+
+  try {
+    var document = XmlService.parse(xml);
+    var root = document.getRootElement()
+    var namespace = root.getNamespace().getURI()
+    var sitemapNameSpace = XmlService.getNamespace(namespace);
+    var items = [];
+    var locs = []
+    if ( root.getChildren('url',sitemapNameSpace)[0] ) {
+      // process only page urls
+      items = root.getChildren('url', sitemapNameSpace);
+      for (var i = 0; i < items.length; i++) {
+        locs.push([items[i].getChild('loc', sitemapNameSpace).getText()]) 
+      }
+    }
+    return locs;
   } catch (e) {
     console.log(e)
     if (e.toString().includes("The markup in the document preceding the root element must be well-formed")) return "Parsing error: is this a valid XML sitemap?";
